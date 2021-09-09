1b9aff8835563811032ec3188c1f279c02f71086
Landers9 <franckaigba4@gmail.com>
2021-09-09 12:31:41 +0100

    error invalid data solved

--- app/mailers/delete_agenda_mailer.rb
+++ app/mailers/delete_agenda_mailer.rb
@@ -1,11 +1,10 @@
 class DeleteAgendaMailer < ApplicationMailer
-    default from: 'from@example.com'
 
     def delete_agenda_mailer(user_delete, email, agenda_title)
         @email = email
         @user_delete = user_delete
         @agenda_title = agenda_title
-        mail to: email, subject: "#{@agenda_title} deleted"
+        mail to: email, subject: "Agenda deleted"
     end
 
 end
